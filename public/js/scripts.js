cf1927d8aeef4034b155cca73a89cd54706064e8
Dean Edwards <dean@web2web.co.za>
2022-07-14 10:52:57 +0200

    converted user actions to use alpine for off and on states

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,13 @@
-// let gridCard = document.querySelectorAll('.grid-card');
-// gridCard.forEach(card => {
-//     let actionsDropdown = card.querySelector('.grid-card__admin-actions').querySelector('button');
-//     actionsDropdown.addEventListener('click', function(e) {
+// let gridCard = $(".grid-card");
+// gridCard.each(function() {
+//     let card = $(this);
+//     let actionsDropdown = card.find('> .grid-card__admin-actions button');
+//     actionsDropdown.on('click', function(e) {
 //         e.preventDefault();
-//         let dropdownMenu = e.target.parentNode.querySelector('.actions-menu');
-//         if(dropdownMenu.classList.contains('hidden')) {
-//             dropdownMenu.classList.remove('hidden')
-//         } else {
-//             dropdownMenu.classList.add('hidden')
-//         }
+//         let actionsMenu = $(e.target).parent().find('.actions-menu');
+//         actionsMenu.hasClass('hidden') ? actionsMenu.removeClass('hidden') : actionsMenu.addClass('hidden');
 //     })
-// });
-let gridCard = $(".grid-card");
-gridCard.each(function() {
-    let card = $(this);
-    let actionsDropdown = card.find('> .grid-card__admin-actions button');
-    actionsDropdown.on('click', function(e) {
-        e.preventDefault();
-        let actionsMenu = $(e.target).parent().find('.actions-menu');
-        actionsMenu.hasClass('hidden') ? actionsMenu.removeClass('hidden') : actionsMenu.addClass('hidden');
-    })
-})
+// })
 
 // TODO Add Delete Prompt on Delete Action
 // let url = "http://127.0.0.1:8000/admin/users/1";
--- resources/views/components/user-actions.blade.php
+++ resources/views/components/user-actions.blade.php
@@ -1,17 +1,19 @@
 @if($currentUser->is_super_admin === 1 || $currentUser->is_admin === 1)
-    <div class="grid-card__admin-actions position-absolute top-2 right-5">
-        <button type="button"
-            class="inline-flex justify-center w-full text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+    <div class="grid-card__admin-actions position-absolute top-2 right-5" x-data="{ open: false }">
+
+        <button x-on:click="open = ! open" type="button"
+            class="inline-flex justify-center w-full text-sm font-medium text-gray-700 focus:outline-none"
             id="menu-button" aria-expanded="true" aria-haspopup="true">
             Options
-            <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg"
+            <svg class="-mr-1 ml-2 h-5 w-5 transition duration-500" xmlns="http://www.w3.org/2000/svg" :class="open ? 'rotate-180' : ''"
                 viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd"
                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                     clip-rule="evenodd" />
             </svg>
         </button>
-        <div class="actions-menu origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+
+        <div x-show="open" x-transition @click.outside="open = false" class="actions-menu origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" style="display: none;"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
                 <a href="#" record="user" person="{{ $user->name }}" record-id="{{ $user->id }}" class="delete text-red-500 block px-4 py-2 text-sm transition duration-300 hover:text-red-700" role="menuitem" tabindex="-1" id="menu-item-0">Delete</a>
